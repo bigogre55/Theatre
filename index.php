22d8bb692d10bd9a55d8ad957d0495e3adfcd045
Jacob Jeffers <30812034+bigogre55@users.noreply.github.com>
2017-08-12 10:43:50 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 require_once("session_start.php");
 include('vars.php');
 ?>
-<title>Jakes Movies</title>
+<title>Theater</title>
 <link rel="stylesheet" type="text/css" href="movie_style.css">
 <link rel="stylesheet" type="text/css" href="../jake.css">
 </head>
 <body>
-<t1><a href="https://movies.jaketheogre.com/test/clear.php">Jakes Movies</a></t1>
+<t1><a href=clear.php">Home</a></t1>
 <p>this portion of the site is still in testing.</p>
 <p>Please be aware that you will find bugs and encounter errors.</p>
 <p>you can always start over by clicking the link above</p>
@@ -28,7 +28,7 @@ $files = scandir("$sdir$mdir");
 
 if ($sd) {
 	echo "<tr>\n";
-        echo "\t<td><a href=\"/test/index.php?up=1\"> . . </a></td>\n";
+        echo "\t<td><a href=\"index.php?up=1\"> . . </a></td>\n";
         echo "</tr>\n";
         }
 
@@ -38,11 +38,11 @@ foreach ($files as $i) {
 //		Do Nothing.
 	} elseif ($h > 0) {
         echo "<tr>\n";
-        echo "\t<td><a href=\"/test/index.php?mov=$i\">$i</a></td>\n";
+        echo "\t<td><a href=\"index.php?mov=$i\">$i</a></td>\n";
         echo "</tr>\n";
 	} else {
 	echo "<tr>\n";
-	echo "\t<td><a href=\"/test/index.php?dir=$i\">$i</a></td>\n";
+	echo "\t<td><a href=\"index.php?dir=$i\">$i</a></td>\n";
 	echo "</tr>\n";
 	}
 }
@@ -50,6 +50,6 @@ foreach ($files as $i) {
 </table>
 </div>
 <hr>
-<p>made by jaketheogre © 2017</p>
+<p>Created by Bigogre55/jaketheogre © 2017</p>
 </body>
 </html>
