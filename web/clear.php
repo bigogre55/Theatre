c85e18459ba3931a6ad5fe411c66308b9d299b7f
Jacob Jeffers <30812034+bigogre55@users.noreply.github.com>
2017-08-12 11:31:16 -0700

    Update clear.php

--- web/clear.php
+++ web/clear.php
@@ -6,5 +6,5 @@ session_write_close();
 setcookie(session_name(),'',0,'/');
 session_regenerate_id(true);
 unset($mdir, $tdir, $homedir);
-header("Location: https://movies.jaketheogre.com/test/");
+header("Location: "https://$host/$uri");
 ?>
